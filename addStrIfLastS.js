eff9919bd834fbcc3359b7d29aea5e8b59e7ae0d
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-06-29 14:54:25 -0400

    Create addStrIfLastS.js

---
+++ addStrIfLastS.js
@@ -0,0 +1,15 @@
+/*
+Write a function called addStringIfLastS() that takes in two parameters.  First, an array of strings and second a single string.  
+Go through each string in the array.  If the string ends in the letter 's' (either case), append your string input to that string in the array.
+Finally, return your modified array.
+
+addStringIfLastS(['test', 'testing', 'testings'], 'TEST')) => ['test', 'testing', 'testingsTEST']
+
+addStringIfLastS(['you', 'should', 'not', 'see'], 'TEST')) //['you', 'should', 'not', 'see']
+*/
+
+function addStrIfLastS(arr, str){
+
+}
+addStrIfLastS(['test', 'testing', 'testings'], 'TEST');
+addStrIfLastS(['you', 'should', 'not', 'see'], 'TEST');
